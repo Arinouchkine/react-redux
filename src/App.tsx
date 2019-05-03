7e70a97e9b1b89681b0c8c5fe020cd6a5b873125
Arinouchkine <arinouchkine@gmail.com>
2019-05-03 13:30:58 +0200

    second

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,38 @@
-import React, { Component } from 'react';
+import React, {Component, FormEvent} from 'react';
 import './App.css';
 
 import Logs from './components/Logs';
 import ItemBox from './components/ItemBox';
+interface AppState {
+  itemBoxSize: number;
+  itemsLabel: string;
+}
 
-export default class App extends Component {
+export default class App extends Component<{}, AppState> {
   state = {
     itemBoxSize: 3,
     itemsLabel: 'ITEM',
+  }
+
+  onChangeSize  = (event: FormEvent<HTMLInputElement>)=>{
+    const currentTarget = event.currentTarget;
+    const value  = parseInt(currentTarget.value, 10);
+    if(isNaN(value))return;
+    this.setState(
+        {
+          itemBoxSize: value
+        }
+    );
+  }
+
+  onChangeText  = (event: FormEvent<HTMLInputElement>)=>{
+    const currentTarget = event.currentTarget;
+    const value  = currentTarget.value;
+    this.setState(
+        {
+          itemsLabel: value
+        }
+    );
   }
 
   render() {
@@ -23,6 +48,7 @@ export default class App extends Component {
           type="number"
           name="itemBoxSize"
           id="itemBoxSize"
+          onChange={this.onChangeSize}
           value={this.state.itemBoxSize}
           />
         <label htmlFor="itemsLabel">Label:</label>
@@ -30,6 +56,7 @@ export default class App extends Component {
           type="text"
           name="itemsLabel"
           id="itemsLabel"
+          onChange={this.onChangeText}
           value={this.state.itemsLabel}
           />
         <ItemBox size={this.state.itemBoxSize} itemsLabel={this.state.itemsLabel} />
